468da3e7e0a08dbb640349cab5408969cb967321
Oleg <oleg.rez@gmail.com>
2025-12-20 20:02:18 +0200

    First commit

---
+++ Main.py
@@ -0,0 +1,26 @@
+import os
+import streamlit as st
+import pandas as pd
+
+file_name = 'ErrorCodes.csv'
+
+if 'error_codes' not in st.session_state:
+    st.session_state['error_codes'] = pd.read_csv(file_name)
+
+error_codes_input = st.chat_input('Enter error codes separated by a comma')
+
+if error_codes_input:
+    error_codes = error_codes_input.split(',')
+
+    if len(error_codes) == 1:
+        df_errors = st.session_state['error_codes']
+        df_result = df_errors[df_errors['Code'] == error_codes[0]]
+
+        if df_result.empty:
+            st.subheader(f'Code {error_codes[0]} not found')
+        else:
+            description = df_result['Description'].iloc[0]
+            st.subheader(f'{error_codes[0]}: {description}')
+
+st.dataframe(st.session_state['error_codes'])
+
